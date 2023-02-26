ded07ef17c506236cde23beaa28ab4b6377e3d44
Yoshifumi Hijikata <yhijikata@systemlancer.com>
2023-02-26 05:46:54 +0000

    Controller化

--- frontend/src/components/page/Contact/Contact.tsx
+++ frontend/src/components/page/Contact/Contact.tsx
@@ -10,8 +10,30 @@ import {
   Typography,
 } from "@mui/material";
 import React from "react";
+import { type SubmitHandler, useForm, Controller } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
+
+interface IFormInput {
+  firstName: string;
+  lastName: string;
+  email: string;
+  contact: string;
+}
 
 const Contact = (): JSX.Element => {
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      lastName: "",
+      firstName: "",
+      email: "",
+      contact: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    console.log(data);
+  };
+
   const theme = createTheme();
   return (
     <ThemeProvider theme={theme}>
@@ -28,60 +50,82 @@ const Contact = (): JSX.Element => {
           <Typography component="h1" variant="h5">
             お問い合わせフォーム
           </Typography>
-          <Box component="form" noValidate sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="姓"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Box sx={{ mt: 3 }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <Controller
+                    name="lastName"
+                    control={control}
+                    render={({ field }) => (
+                      <TextField
+                        fullWidth
+                        {...field}
+                        label="姓"
+                        autoComplete="family-name"
+                        autoFocus
+                      />
+                    )}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Controller
+                    name="firstName"
+                    control={control}
+                    render={({ field }) => (
+                      <TextField
+                        fullWidth
+                        {...field}
+                        label="名"
+                        autoComplete="given-name"
+                      />
+                    )}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Controller
+                    name="email"
+                    control={control}
+                    render={({ field }) => (
+                      <TextField
+                        fullWidth
+                        {...field}
+                        label="メールアドレス"
+                        autoComplete="email"
+                      />
+                    )}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Controller
+                    name="contact"
+                    control={control}
+                    render={({ field }) => (
+                      <TextField
+                        {...field}
+                        label="お問い合わせ内容"
+                        placeholder="お問い合わせ内容をご入力ください"
+                        multiline
+                        fullWidth
+                        rows={4}
+                      />
+                    )}
+                  />
+                </Grid>
               </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="名"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="メールアドレス"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  label="お問い合わせ内容"
-                  placeholder="お問い合わせ内容をご入力ください"
-                  multiline
-                  fullWidth
-                  rows={4}
-                />
-              </Grid>
-            </Grid>
-          </Box>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            送信
-          </Button>
+            </Box>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              送信
+            </Button>
+          </form>
         </Box>
       </Container>
+      <DevTool control={control} />
     </ThemeProvider>
   );
 };
