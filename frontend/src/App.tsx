8b16646a3fb7f85f3c7a726a5629cf36a5d76dad
Yoshifumi Hijikata <yhijikata@systemlancer.com>
2023-02-27 09:34:42 +0000

    #4

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Grid from "@mui/material/Grid";
+import Header from "./components/Header/Header";
 
 function App(): JSX.Element {
+  const [title, setTitle] = useState<string | null>(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    const parsedTitle = location.pathname.replace(/\W/g, " ");
+    setTitle(parsedTitle);
+  }, [location]);
+
   return (
     <Grid container>
       <Navbar />
+      <Header title={title} />
       <Outlet />
     </Grid>
   );
--- frontend/src/pages/Authentication/Authentication.tsx
+++ frontend/src/pages/Authentication/Authentication.tsx
@@ -1,39 +1,8 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
-import CommonButton from "../../components/common/CommonButton/CommonButton";
 
 const Authentication = (): JSX.Element => {
-  const buttonStyles = {
-    fontSize: "0.875rem",
-    fontWeight: 600,
-    textTransform: "capitalize",
-    borderRadius: 2.5,
-    "&.MuiButton-contained": {
-      backgroundColor: "#009be5",
-      "&:hover": {
-        backgroundColor: "#006db3",
-      },
-    },
-    "&.MuiButton-outlined": {
-      color: "#fff",
-      borderColor: "#fff",
-      "&:hover": {
-        backgroundColor: "transparent",
-      },
-    },
-  };
-
-  return (
-    <Grid item xs={8}>
-      This is authentication page.
-      <CommonButton sx={buttonStyles} variant="contained">
-        Add user
-      </CommonButton>
-      <CommonButton sx={buttonStyles} variant="outlined">
-        Add user
-      </CommonButton>
-    </Grid>
-  );
+  return <Grid item xs={8}></Grid>;
 };
 
 export default Authentication;
